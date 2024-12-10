8d8473ae2c69124db026ba065d87d21ea018e412
howard-ch-wang <chwangenfrance@gmail.com>
2024-12-10 05:12:59 +0100

    add more weather features

--- test_features.py
+++ test_features.py
@@ -27,13 +27,54 @@ def _merge_external_data(X):
         .interpolate(method="linear") #test with and without
     )
 
-    weather["is_rain"] = (weather["RR1"] > 0).astype(int)
-    weather['is_snow'] = (weather['NEIGETOT'] > 0).astype(int)
+    q_indicators = [col for col in weather.columns if col.startswith('Q')]
+    w_values = weather.drop(columns=q_indicators)
 
-    cols = ['RR1', 'FF', 'T', 'TD', 'U', 'NEIGETOT', 'is_rain', 'is_snow']
+    cols = ['RR1', 'DRR1', 'FF', 'FXY', 'FXI', 'FXI3S', 
+            'T', 'TD', 'TN', 'TX', 'DG', 'U', 'UX', 'DHUMI40',
+            'DHUMI80', 'INS', 'VV', 'DVV200', 'NEIGETOT']
+    # cols = w_values.columns
 
-    X = X.merge(weather[cols], on='date', how='left')
+    X = X.merge(w_values[cols], on='date', how='left')
     return X
+
+def create_features(X):
+    X["is_rain"] = (X["RR1"] > 0).astype(int)
+    X['is_snow'] = (X['NEIGETOT'] > 0).astype(int)
+    X['wind_chill'] = 13.12 + 0.6215 * X['T'] - 11.37 * (X['FF']*3.6)**0.16 + 0.3965 * X['T'] * (X['FF']*3.6)**0.16
+    X['is_rush_hour'] = (
+        (X['date'].dt.hour >= 8) & (X['date'].dt.hour < 10) | 
+        (X['date'].dt.hour >= 17) & (X['date'].dt.hour < 20)
+    ).astype(int)
+
+    return X
+
+def covid_dates(df):
+
+    '''Creates a binary variable - 1 if that observation happened during a lockdown in paris and 0 otherwise
+    '''
+
+    X = df.copy()
+
+    date_ranges = [
+    #("2020-03-17", "2020-05-11"),
+    ("2020-10-05", "2020-12-14"),
+    ("2021-03-20", "2021-05-03"),
+    ]
+
+    # Convert date ranges to pandas datetime
+    date_ranges = [(pd.to_datetime(start), pd.to_datetime(end)) for start, end in date_ranges]
+
+    # Create binary variable
+    def is_in_date_range(date, ranges):
+        for start, end in ranges:
+            if start <= date <= end:
+                return 1
+        return 0
+
+    X["in_date_range"] = X["date"].apply(lambda x: is_in_date_range(x, date_ranges))
+    return X
+
 
 
 # X, y = utils.get_train_data()
